243b0c562c15c27d618bd99099a651e0de20682b
AndreyMarkinPPC <andrey.markin.ppc@gmail.com>
2020-04-19 17:46:29 +0100

    Add saving turns
    The current turn is saved in `iter.txt` file - this allows to switch
    between players when saying *Twister next round*

--- __init__.py
+++ __init__.py
@@ -1,19 +1,18 @@
 from mycroft import MycroftSkill, intent_file_handler
 from random import randint
+import os
+from os.path import expanduser
 
-# global i = 0
-players = ['Andrey', 'Katya']
+home = expanduser("~")
+players = ['Andrei', 'Katja']
 parts = ['right hand', 'left hand', 'right foot', 'left foot']
 colors = ['blue', 'green', 'yellow', 'red', 'air']
 
 def r_generator(max_int):
     return randint(0, max_int)
 
-def spin_twister():
-    # global i
-    i = 0
+def spin_twister(i):
     res = "Player %s, put %s on the %s" % (players[i%2], parts[r_generator(3)], colors[r_generator(4)])
-    # i+=1
     return res
 
 class TwisterPlayer(MycroftSkill):
@@ -23,16 +22,25 @@ class TwisterPlayer(MycroftSkill):
     @intent_file_handler('start.twister.intent')
     def handle_start_twister(self, message):
         self.speak_dialog('start.twister')
+        # os.system("echo 0 > ~/iter.txt")
+        open(os.path.join(home, "iter.txt"), "w").write('0')
+
 
     @intent_file_handler('stop.twister.intent')
     def handle_stop_twister(self, message):
         self.speak_dialog('stop.twister')
+        os.system("rm ~/iter.txt")
 
     @intent_file_handler('spin.twister.intent')
     def handle_spin_twister(self, message):
         self.speak_dialog('spin.twister')
-        action = spin_twister()
+        iter = int(open(os.path.join(home, "iter.txt"), "r").read())
+        action = spin_twister(iter)
         self.speak_dialog('action.twister', {'action': action})
+        os.system("echo %s > ~/last_action.txt" % action)
+        iter +=1
+        open(os.path.join(home, "iter.txt"), "w").write(str(iter))
+        
 
 
 def create_skill():
